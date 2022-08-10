efcc671f14dad79708a8630eed23ea706ba214ce
ThanhLVu <12865705@student.uts.edu.au>
2022-08-10 17:36:03 +1000

    Adds 'usable' values for speed and acceleration

--- hans_cute_driver/src/main.cpp
+++ hans_cute_driver/src/main.cpp
@@ -21,6 +21,8 @@ int main(int argc, char *argv[])
   {
     servo_ids.push_back(i);
     hans_robot.setTorqueEnable(i, true);
+    hans_robot.setAcceleration(i, 20);
+    hans_robot.setSpeed(i,300);
 
     unsigned int position = 0;
     hans_robot.getPosition(i, position);
@@ -28,9 +30,11 @@ int main(int argc, char *argv[])
     hans_robot.setPosition(i, 2048);
   }
 
-  // std::this_thread::sleep_for(std::chrono::milliseconds((2500)));
+  std::this_thread::sleep_for(std::chrono::milliseconds((2500)));
   // hans_robot.setMultiSpeed(servo_ids,speeds);
-  // hans_robot.setPosition(5, 3000);
+  hans_robot.setPosition(5, 3000);
+  hans_robot.setAcceleration(5, 20);
+  hans_robot.setSpeed(5,300);
 
   // std::this_thread::sleep_for(std::chrono::milliseconds((2500)));
   // hans_robot.setMultiSpeed(servo_ids,speeds);
