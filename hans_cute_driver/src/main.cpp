c74129e2a37d45dde4e869a92a433c8935c052c8
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2022-08-10 14:17:21 +0700

    Reopened example code in driver

--- hans_cute_driver/src/main.cpp
+++ hans_cute_driver/src/main.cpp
@@ -13,32 +13,20 @@ int main(int argc, char *argv[])
   hans_robot.setSerialPort(serial_port);
   hans_robot.open();
 
-  // std::vector<unsigned int> servo_ids;
+  std::vector<unsigned int> servo_ids;
   // std::vector<unsigned int> positions;
   // std::vector<unsigned int> speeds;
 
-  // for (int i = 0; i <= 6; i++)
-  // {
-  //   servo_ids.push_back(i);
-  //   positions.push_back(2048);
-  //   speeds.push_back(1023);
+  for (int i = 0; i <= 6; i++)
+  {
+    servo_ids.push_back(i);
+    hans_robot.setTorqueEnable(i, true);
 
-  //   unsigned int max_torque = 0;
-  //   hans_robot.getMaxTorque(i,max_torque);
-  //   std::cout << max_torque << std::endl;
-
-  //   hans_robot.setTorqueEnable(i, true);
-
-  //   unsigned int position = 0;
-  //   hans_robot.getPosition(i, position);
-  //   hans_robot.setPosition(i, position);
-  //   hans_robot.setSpeed(i,512);
-  //   unsigned int speed = 0;
-  //   hans_robot.getSpeed(i, speed);
-  //   // std::cout << speed << std::endl;
-  //   hans_robot.setTorqueLimit(i,1);
-  //   hans_robot.setPosition(i, 2048);
-  // }
+    unsigned int position = 0;
+    hans_robot.getPosition(i, position);
+    hans_robot.setPosition(i, position);
+    hans_robot.setPosition(i, 2048);
+  }
 
   // std::this_thread::sleep_for(std::chrono::milliseconds((2500)));
   // hans_robot.setMultiSpeed(servo_ids,speeds);
@@ -48,6 +36,6 @@ int main(int argc, char *argv[])
   // hans_robot.setMultiSpeed(servo_ids,speeds);
   // hans_robot.setPosition(5, 900);
 
-  // hans_robot.close();
+  hans_robot.close();
   return 0;
 }
